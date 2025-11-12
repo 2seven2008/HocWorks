0580b411904ed7130c1a89a5627f100955a8fa94
FelipeNovais <162530029+KanekaLipe@users.noreply.github.com>
2025-11-12 12:11:32 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,22 +2,22 @@ const CACHE_HOC = 'meu-pwa-cache-hoc';
 const urlsToCache = [
     '/',
     '/index.html',
-    '/empresa.html',
-    '/contato.html',
-    './css/style.css',
-    './js/script.js'
+    'empresa.html',
+    'contato.html',
+    'css/style.css',
+    'js/script.js'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_HOC)
-            .then(cache => cache.addAll(urlsToCache))
+        .then(cache => cache.addAll(urlsToCache))
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        .then(response => response || fetch(event.request))
     );
 });
