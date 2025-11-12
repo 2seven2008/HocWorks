5038f8268db65d9b4ad49288b08edb0b53cdb8f3
2seven <168299805+2seven2008@users.noreply.github.com>
2025-11-12 19:28:02 -0300

    UPDATE MAXIMEZED
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,27 @@
-const CACHE_HOC = 'hoc-works-cache';
-const urlsToCache = [
+const CACHE_NAME = 'hoc-works-cache';
+const ASSETS_TO_CACHE = [
     './',
-    '/index.html',
-    '/empresa.html',
-    '/contato.html',
-    '/css/style.css',
-    '/js/script.js',
-    '/img/icones/icones_png/icone_192.png',
-    '/img/icones/icones_png/icone_512.png'
+    './index.html',
+    './empresa.html',
+    './contato.html',
+    './css/style.css',
+    './js/script.js',
+    './img/icones/icones_png/icone_192.png',
+    './img/icones/icones_png/icone_512.png'
 ];
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_HOC).then((cache) => {
-            cache.addAll(urlsToCache);
+        caches.open(CACHE_NAME).then((cache) => {
+            cache.addAll(ASSETS_TO_CACHE);
         })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
             return response || fetch(event.request);
         })
     );
-
 });
-
